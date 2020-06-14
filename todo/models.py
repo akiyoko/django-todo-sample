c0acef8d62ca741d74d50e97809ab3e0c4ea3cc9
akiyoko <akiyoko@users.noreply.github.com>
2020-06-15 05:55:30 +0900

    コメント修正

--- todo/models.py
+++ todo/models.py
@@ -9,6 +9,7 @@ class Todo(models.Model):
         verbose_name = verbose_name_plural = 'TODO'
 
     title = models.CharField(verbose_name='タイトル', max_length=255)
+    # description = models.TextField(verbose_name='詳細', null=True, blank=True)
     expiration_date = models.DateField(verbose_name='期限日', null=True, blank=True)
 
     def __str__(self):
--- todo/views.py
+++ todo/views.py
@@ -8,45 +8,45 @@ from .forms import TodoForm
 from .models import Todo
 
 
-# No.1: はじめての画面表示
-def todo_list(request):
-    if request.method == 'GET':
-        today = timezone.localdate()
-        return HttpResponse('今日は {} です。'.format(today))
+# Step 1. はじめての画面表示
+# def todo_list(request):
+#     if request.method == 'GET':
+#         today = timezone.localdate()
+#         return HttpResponse('今日は {} です。'.format(today))
 
 
-# No.2: クラスベースで書き直す
-class TodoListView(View):
-    def get(self, request, *args, **kwargs):
-        today = timezone.localdate()
-        return HttpResponse('今日は {} です。'.format(today))
+# Step 2. クラスベースで書き直す
+# class TodoListView(View):
+#     def get(self, request, *args, **kwargs):
+#         today = timezone.localdate()
+#         return HttpResponse('今日は {} です。'.format(today))
 
 
-# No.3: HTMLを使ったレスポンスを返す
-class TodoListView(View):
-    def get(self, request, *args, **kwargs):
-        today = timezone.localdate()
-        return HttpResponse("""
-<html>
-<body>
-<h1>TODOリスト</h1>
-<p>今日は {} です。</p>
-</body>
-</html>
-        """.format(today))
+# Step 3. HTMLを使ったレスポンスを返す
+# class TodoListView(View):
+#     def get(self, request, *args, **kwargs):
+#         today = timezone.localdate()
+#         return HttpResponse("""
+# <html>
+# <body>
+# <h1>TODOリスト</h1>
+# <p>今日は {} です。</p>
+# </body>
+# </html>
+#         """.format(today))
 
 
-# No.4: テンプレートを使う
-class TodoListView(View):
-    def get(self, request, *args, **kwargs):
-        today = timezone.localdate()
-        context = {
-            'today': today,
-        }
-        return TemplateResponse(request, 'todo/todo_list.html', context)
+# Step 4. テンプレートを使う
+# class TodoListView(View):
+#     def get(self, request, *args, **kwargs):
+#         today = timezone.localdate()
+#         context = {
+#             'today': today,
+#         }
+#         return TemplateResponse(request, 'todo/todo_list_step4.html', context)
 
 
-# No.5: モデルを使う
+# Step 5. モデルを使う
 class TodoListView(View):
     def get(self, request, *args, **kwargs):
         today = timezone.localdate()
@@ -55,22 +55,25 @@ class TodoListView(View):
             'today': today,
             'todo_list': todo_list,
         }
+        # return TemplateResponse(request, 'todo/todo_list_step5.html', context)
         return TemplateResponse(request, 'todo/todo_list.html', context)
 
 
+# Step 6. TODO追加画面を作成する
 class TodoCreateView(View):
     def get(self, request, *args, **kwargs):
+        # 空のフォームを作成して画面に表示
         context = {
             'form': TodoForm(),
         }
         return TemplateResponse(request, 'todo/todo_create.html', context)
 
     def post(self, request, *args, **kwargs):
-        # リクエストからフォームを作成
+        # リクエストパラメータからフォームを作成
         form = TodoForm(request.POST)
-        # バリデーション
+        # フォームを使ってバリデーション
         if not form.is_valid():
-            # バリデーションNGの場合は追加画面のテンプレートを再表示
+            # バリデーションNGの場合はリクエスト元の画面のテンプレートを再表示
             context = {
                 'form': form,
             }
@@ -78,37 +81,43 @@ class TodoCreateView(View):
 
         # オブジェクトを保存
         form.save()
+        # TODOリスト画面にリダイレクト
         return HttpResponseRedirect('/todo/')
 
 
+# Step 7. TODO変更画面を作成する
 class TodoUpdateView(View):
     def get(self, request, pk, *args, **kwargs):
+        # 対象レコードを取得
         try:
             todo = Todo.objects.get(pk=pk)
         except Todo.DoesNotExist:
             raise Http404
 
+        # 対象レコードからフォームを作成して画面に表示
         context = {
             'form': TodoForm(instance=todo),
         }
         return TemplateResponse(request, 'todo/todo_update.html', context)
 
     def post(self, request, pk, *args, **kwargs):
+        # 対象レコードを取得
         try:
             todo = Todo.objects.get(pk=pk)
         except Todo.DoesNotExist:
             raise Http404
 
-        # リクエストからフォームを作成
+        # リクエストパラメータと対象レコードからフォームを作成
         form = TodoForm(request.POST, instance=todo)
-        # バリデーション
+        # フォームを使ってバリデーション
         if not form.is_valid():
-            # バリデーションNGの場合は追加画面のテンプレートを再表示
+            # バリデーションNGの場合はリクエスト元の画面のテンプレートを再表示
             context = {
                 'form': form,
             }
             return TemplateResponse(request, 'todo/todo_update.html', context)
 
-        # オブジェクトを保存
+        # オブジェクトを更新
         form.save()
+        # TODOリスト画面にリダイレクト
         return HttpResponseRedirect('/todo/')
